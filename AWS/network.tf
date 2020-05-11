cb9f36cb5f0557fd1706887f735f27db1b383dfc
Eric Gottesman <egottesman@crystald.com>
2020-05-10 17:11:23 -0700

    Fixed routing to internet, removed unused EIP

--- AWS/network.tf
+++ AWS/network.tf
@@ -1,36 +1,33 @@
 resource "aws_vpc" "jamulus" {
-  cidr_block         = "172.16.0.0/16"
+  cidr_block               = "172.16.0.0/16"
 
-  tags               = {
-    Name             = "jamulus-vpc"
-    Application      = "jamulus"
+  tags                     = {
+    Name                   = "jamulus-vpc"
+    Application            = "jamulus"
   }
 }
 
 resource "aws_subnet" "jamulus_subnet" {
-    vpc_id           = aws_vpc.jamulus.id
-    cidr_block       = "172.16.0.0/24"
+    vpc_id                 = aws_vpc.jamulus.id
+    cidr_block             = "172.16.0.0/24"
 
-    tags             = {
-      Name           = "jamulus-subnet"
-      Application    = "jamulus"
+    tags                   = {
+      Name                 = "jamulus-subnet"
+      Application          = "jamulus"
     }
 }
 
 resource "aws_internet_gateway" "jamulus_internet" {
-  vpc_id             = aws_vpc.jamulus.id
+    vpc_id                 = aws_vpc.jamulus.id
 
-  tags               = {
-    Name             = "jamulus-internet-gateway"
-    Application      = "jamulus"
-  }
+    tags                   = {
+      Name                 = "jamulus-internet-gateway"
+      Application          = "jamulus"
+    }
 }
 
-resource "aws_eip" "access_eip" {
-  vpc                = true
-
-  tags               = {
-    Name             = "jamulus-ip"
-    Application      = "jamulus"
-  }
+resource "aws_route" "route_to_internet" {
+    route_table_id         = aws_vpc.jamulus.default_route_table_id
+    destination_cidr_block = "0.0.0.0/0"
+    gateway_id             = aws_internet_gateway.jamulus_internet.id
 }
