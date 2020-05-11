c55aa9c4c64581c1bb01326bce84005bdb8f6184
Eric Gottesman <egottesman@crystald.com>
2020-05-10 17:31:41 -0700

    Added health check security group

--- AWS/security_groups.tf
+++ AWS/security_groups.tf
@@ -26,15 +26,43 @@ resource "aws_security_group" "jamulus" {
   }
 }
 
+resource "aws_security_group" "jamulus_health" {
+  name          = "jamulus-health"
+  description   = "Health check access for load balancers."
+  vpc_id        = aws_vpc.jamulus.id
+
+  ingress {
+    from_port   = "22124"
+    to_port     = "22124"
+    protocol    = "tcp"
+    cidr_blocks = [
+      aws_subnet.jamulus_subnet.cidr_block
+    ]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = [
+      "0.0.0.0/0"
+    ]
+  }
+
+  tags          = {
+    Application = "jamulus"
+  }
+}
+
 resource "aws_security_group" "ssh" {
   name          = "jamulus-ssh"
   description   = "SSH access to Jamulus through NLB."
   vpc_id        = aws_vpc.jamulus.id
 
   ingress {
-    from_port = 22
-    to_port = 22
-    protocol = "tcp"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
     cidr_blocks = local.sshblocks
   }
 
