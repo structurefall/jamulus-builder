be90d03d94258dfbef512b52d98627ae7428559e
Eric Gottesman <egottesman@crystald.com>
2020-05-12 18:10:58 -0700

    changing state bucket to use account ID for uniqueness

--- AWS/Initializer/main.tf
+++ AWS/Initializer/main.tf
@@ -3,8 +3,10 @@ provider "aws" {
   profile          = "personal"
 }
 
+data "aws_caller_identity" "current" {}
+
 resource "aws_s3_bucket" "jamulus_terraform_state_bucket" {
-    bucket = "terraform-jamulus"
+    bucket = "terraform-jamulus-${data.aws_caller_identity.current.account_id}"
     region = "us-west-1"
 }
 
